d704e14e54b59737e48a05595d56af7620b3662d
JoshuaDG <JoshuaDG@users.noreply.github.com>
2018-01-26 12:18:02 -0800

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -1,1 +1,16 @@
 
+"""
+@author: JoshuaDGonzalez
+project:assignmnet two
+"""
+
+import numpy as np
+
+a = np.random.rand(4,2)
+
+print(a)
+
+#loop  through the matrix two times, print one colum than the next
+for i in range(2):
+    for b in range(4):
+        print(a[b][i])
